07ef062a7bba61d532906556ced2a9cbb499a936
Dushant Singh <dushant.waora@gmail.com>
2017-05-20 11:19:01 +0200

    Commons provided URL paths and common functions added

--- src/main/java/com/dushantsw/utilities/Commons.java
+++ src/main/java/com/dushantsw/utilities/Commons.java
@@ -1,10 +1,39 @@
 package com.dushantsw.utilities;
 
+import java.util.UUID;
+
 /**
  * <code>Commons</code>
  *
  * @author dushantsw
  */
 public class Commons {
+    /**
+     * Base url to the wikipedia api
+     */
+    public static final String WIKIPEDIA_BASE_URL = "https://en.wikipedia.org/w/api.php";
+    /**
+     * Base url to the music brainz url
+     */
+    public static final String MUSICBRAINZ_BASE_URL = "http://musicbrainz.org/ws/2/artist/";
+    /**
+     * Base URL to the cover art archive url.
+     */
+    public static final String COVER_ART_BASE_URL = "http://coverartarchive.org/release-group/";
 
+    /**
+     * Checks if the given UUID is parsable with {@link UUID} class.
+     *
+     * @param uuid string format uuid.
+     * @return True if parsable else false.
+     */
+    public static boolean isUUIDValid(String uuid) {
+        try {
+            //noinspection ResultOfMethodCallIgnored
+            UUID.fromString(uuid);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
