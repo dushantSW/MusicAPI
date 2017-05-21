c7d5d46afb5a8913e7d7fe6e833ab1598ea6cd48
Dushant Singh <dushant.waora@gmail.com>
2017-05-21 12:21:02 +0200

    Cover and wikipedia tests for synchornous requests.

--- src/test/java/com/dushantsw/integration/managers/CoverArtTest.java
+++ src/test/java/com/dushantsw/integration/managers/CoverArtTest.java
@@ -8,81 +8,47 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.fail;
-
 /**
  * <code>CoverArtTest</code>
  *
  * @author dushantsw
  */
 public class CoverArtTest {
-    private CountDownLatch lock;
     private CoverArtArchiveClient client;
-    private boolean status = false;
 
     @Before
     public void beforeTests() {
-        status = false;
-        lock = new CountDownLatch(1);
         client = new DefaultCoverArtArchiveClient();
     }
 
     @Test(expected = InvalidMBIDException.class)
     public void testNullOrEmptyMbId() throws Throwable {
-        client.getCoverImagesByMBID(null, null);
+        client.getCoverImagesByMBID(null);
     }
 
     @Test(expected = InvalidMBIDException.class)
     public void testInvalidUUIDMBID() throws Throwable {
-        client.getCoverImagesByMBID("asdas-asdasgsgsdgsdgsdgsdgsgsdgs", null);
+        client.getCoverImagesByMBID("asdas-asdasgsgsdgsdgsdgsdgsgsdgs");
     }
 
-    @Test
+    @Test(expected = ImageRetrievingException.class)
     public void testNotAvailableCoverArt() throws Throwable {
         // Checked uuid, it should throw 404 and get caught by client.
         String INVALID_MBID = "1b023e01-4d56-387b-8758-8678046e4cef";
-        client.getCoverImagesByMBID(INVALID_MBID, new CoverArtCallback() {
-            @Override
-            public void onCovertArtRetrievingFinished(Images images) {
-                fail();
-            }
-
-            @Override
-            public void onCovertArtRetrievingFailed(ImageRetrievingException ex) {
-                Assert.assertNotNull(ex);
-                Assert.assertEquals("cover_art_not_found", ex.getMessage());
-                lock.countDown();
-                status = true;
-            }
-        });
-
-        lock.await(10, TimeUnit.SECONDS);
-        Assert.assertTrue(status);
+        Images images = client.getCoverImagesByMBID(INVALID_MBID);
+        Assert.assertNotNull(images);
+        Assert.assertEquals(3, images.size());
+        images.forEach(image -> Assert.assertNotNull(image.getImageURL().getUrl()));
     }
 
     @Test
-    public void testSuccessCovertDownload() throws Throwable {
+    public void testSuccessCoverDownload() throws Throwable {
         String RELEASE_MBID = "1b022e01-4da6-387b-8658-8678046e4cef";
-        client.getCoverImagesByMBID(RELEASE_MBID, new CoverArtCallback() {
-            @Override
-            public void onCovertArtRetrievingFinished(Images images) {
-                Assert.assertNotNull(images);
-                Assert.assertEquals(3, images.size());
-                images.forEach(image -> Assert.assertNotNull(image.getImageURL().getUrl()));
-                lock.countDown();
-                status = true;
-            }
+        System.out.printf("Release mbid: " + RELEASE_MBID);
 
-            @Override
-            public void onCovertArtRetrievingFailed(ImageRetrievingException ex) {
-                fail();
-            }
-        });
-
-        lock.await(10, TimeUnit.SECONDS);
-        Assert.assertTrue(status);
+        Images images = client.getCoverImagesByMBID(RELEASE_MBID);
+        Assert.assertNotNull(images);
+        Assert.assertEquals(3, images.size());
+        images.forEach(image -> Assert.assertNotNull(image.getImageURL().getUrl()));
     }
 }
